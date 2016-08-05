40cb8598c10e707a2c4518d3d542bd129da25261
Harshil Darji <darjiharshil2994@gmail.com>
2016-08-05 10:04:51 +0530

    Improvement

--- Compare.py
+++ Compare.py
@@ -1,28 +1,20 @@
 # Compare Two CSV Files
 
-fName1 = "Sample1.csv"          # Name of first .csv file
-fName2 = "Sample2.csv"          # Name of second .csv file
+fList = ['Sample1.csv','Sample2.csv']       # List of file names
+file1 = []                                  # First list to store data of first .csv file
+file2 = []                                  # Second list to store data of second .csv file
 
-file1 = []                      # First list to store data of first .csv file
-file2 = []                      # Second list to store data of second .csv file
-
-for line in open(fName1):       # Loop through each line in first .csv file
-    fields = line.split(",")    # Extract items seperated by ','
-    name = fields[0]
-    enr = fields[1]
-    spi = fields[2]
-    add = fields[3][:-1]
-    s = (name, enr, spi, add)   # Inserting data into tuple
-    file1.append(s)             # Append tuple into file1
-    
-for line in open(fName2):       # Loop through each line in second .csv file
-    fields = line.split(",")    # Extract items seperated by ','
-    name = fields[0]
-    enr = fields[1]
-    spi = fields[2]
-    add = fields[3][:-1]
-    s = (name, enr, spi, add)   # Inserting data into tuple
-    file2.append(s)             # Append tuple into file2
+for i in range(2):
+    fName = fList[i]                        # Select file
+    for line in open(fName):                # Loop through each line in first .csv file
+        fields = line.split(',')            # Extract tems seperated by ','
+        name = fields[0]
+        enr = fields[1]
+        spi = fields[2]
+        add = fields[3]
+        s = (name, enr, spi, add)           # Insert data into tuple
+        if i == 0:      file1.append(s)     # Append tuple into file
+        elif i == 1:    file2.append(s)     # Append tuple into file
 
 output = open("Output.csv","w")
 for line in file1:              # Loop through each line in 'file1'
@@ -32,6 +24,6 @@ for line in file1:              # Loop through each line in 'file1'
          if count < 3:          # Count should be less than 'Total Columns - 1'
              print(item, file=output, end=",")
          else:
-             print(item, file=output, end="\n")
+             print(item, file=output, end="")
          count += 1
 output.close()
